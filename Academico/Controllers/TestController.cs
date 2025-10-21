27c6364b84cc582ab7a716cdf4b07b25ca7887d5
brunett06 <brunacassiane06@gmail.com>
2025-10-21 19:49:38 -0300

    Aula

---
+++ Academico/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Academico.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+            public IActionResult Aula()
+        {
+            return View();
+        }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
